65b0d90fa0bbf738385964a0a6ee2585ea76eb95
Ashun007 <157597734+Nautiyal-Ashish@users.noreply.github.com>
2024-05-07 09:50:39 +0530

    02_Create own react and JSX

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,5 +1,15 @@
 function App() {
-  return <h1>Chai aur React with vite </h1>;
+  const username = "chai aur code";
+
+  return (
+    <>
+      <h1>
+        Chai aur React with {/* evaluated expression */}
+        {username}
+      </h1>
+      <p>Hi i am Para</p>
+    </>
+  );
 }
 
 export default App;
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// ----------------------------
+// function inside the main
+function MyApp() {
+  return (
+    <>
+      <h1>Hello World</h1>;
+    </>
+  );
+}
+// ---------------------------
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit Google
+  </a>
 );
+
+// ------------------------------
+// Creatting Element Syntax
+const anotherUser = "Chai aur react";
+
+const reactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "Click Me to visit google",
+  anotherElement
+);
+// --------------------------------
+
+ReactDOM.createRoot(document.getElementById("root")).render(reactElement);
