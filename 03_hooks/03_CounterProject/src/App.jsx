ccc67b508f28da93b0ddcce12ed98b77efb9b3b6
Ashun007 <157597734+Nautiyal-Ashish@users.noreply.github.com>
2024-05-07 10:22:19 +0530

    03 _ hooks and Counter Project

---
+++ 03_hooks/03_CounterProject/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  // can give any name in counter and setCounter
+  let [counter, setCounter] = useState(5);
+
+  // let counter = 5;
+  const addValue = () => {
+    // ----------------------
+    //here in console number is inscreasing but in website it is not updating(ui updation not happening)
+    // console.log("Clicked", counter);
+    // -----------------------
+    counter = counter + 1;
+    setCounter(counter);
+    // -----------------------
+    // or setCounter(counter + 1)
+  };
+
+  const removeValue = () => {
+    counter = counter - 1;
+    setCounter(counter);
+  };
+
+  return (
+    <>
+      <h1>Chai Aur React</h1>
+      <h2>Counter Value : {counter}</h2>
+
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  );
+}
+
+export default App;
