9415eb5609ecc9b4690ed6fdfcab8947b71379ae
Ashun007 <157597734+Nautiyal-Ashish@users.noreply.github.com>
2024-05-12 09:24:28 +0530

    Project Updated components and store

--- 13_AppwriteMegaBlogProject/src/App.jsx
+++ 13_AppwriteMegaBlogProject/src/App.jsx
@@ -1,17 +1,41 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  // console.log(process.env.REACT_APP_APPEWRITE_URL); //this is create react app 
-  
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  // Loading State
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
-
-  return (
-    <>
-      <h1>Blog Webiste with Appwrite</h1>
-    </>
-  );
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
